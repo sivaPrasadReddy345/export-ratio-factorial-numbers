3428c9bfe0b6f24b495e7480e904b42bd235c5c5
Siva prasad reddy <siva.06528@gmail.com>
2021-10-30 05:41:51 +0000

    ratio and factorial of numbers

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,11 @@
+const factorialOfNumbers = require("../factorial/index.js");
+const ratioOfTwoNumbers = require("../ratio/index.js");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumbers(num3);
+
+  return { ratio, factorial };
+};
+
+module.exports = ratioAndFactorial;
